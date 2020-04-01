70916511e2cb102fceee3a97661a185a7a3bcfa4
fthillen <63006613+fthillen@users.noreply.github.com>
2020-04-01 20:53:18 +0200

    Update powerline.js

--- powerline.js
+++ powerline.js
@@ -1,3 +1,20 @@
+black       = '#000000';
+red         = '#ed1e1e'; // red
+green       = '#afd52a'; // green
+yellow      = '#fead2a'; // yellow
+blue        = '#1487ad'; // blue
+magenta     = '#d45f1b'; // orange
+cyan        = '#d45f1b'; // orange
+white       = '#d0d0d0'; // light gray
+lightBlack  = '#0b0b0b'; // medium gray
+lightRed    = '#ed1e1e'; // red
+lightGreen  = '#afd52a'; // green
+lightYellow = '#fead2a'; // yellow
+lightBlue   = '#1487ad'; // blue
+lightMagenta= '#d45f1b'; // orange
+lightCyan   = '#d45f1b'; // orange
+lightWhite  = '#fcfcfc'; // white
+
 t.prefs_.set('color-palette-overrides',
                  [ black , red     , green  , yellow,
                   blue     , magenta , cyan   , white,
