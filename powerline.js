fa9d7f541824df63f11fb411033fa726761ca0da
fthillen <63006613+fthillen@users.noreply.github.com>
2020-04-01 21:09:25 +0200

    Update powerline.js

--- powerline.js
+++ powerline.js
@@ -1,19 +1,19 @@
 black       = '#000000';
 red         = '#ed1e1e'; // red
 green       = '#afd52a'; // green
-yellow      = '#fead2a'; // yellow
+yellow      = '#d45f1b'; // yellow
 blue        = '#1487ad'; // blue
-magenta     = '#d45f1b'; // orange
-cyan        = '#d45f1b'; // orange
+magenta     = '#fead2a'; // orange
+cyan        = '#fead2a'; // orange
 white       = '#d0d0d0'; // light gray
 lightBlack  = '#0b0b0b'; // medium gray
 lightRed    = '#ed1e1e'; // red
 lightGreen  = '#afd52a'; // green
-lightYellow = '#fead2a'; // yellow
+lightYellow = '#d45f1b'; // yellow
 lightBlue   = '#1487ad'; // blue
 lightMagenta= '#d45f1b'; // orange
-lightCyan   = '#d45f1b'; // orange
-lightWhite  = '#fcfcfc'; // white
+lightCyan   = '#fead2a'; // orange
+lightWhite  = '#fead2a'; // white
 
 t.prefs_.set('color-palette-overrides',
                  [ black , red     , green  , yellow,
